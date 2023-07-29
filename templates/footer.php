470ded7a0c0d1aa60795086ee3a40fda84b8365f
ihsavru <vurvashi11@gmail.com>
2023-07-29 23:40:10 +0530

    Initial commit

---
+++ templates/footer.php
@@ -0,0 +1,60 @@
+<footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h3>Company</h3>
+            <p><strong>Address:</strong>Unit No-512 AND 512 A, Plot No. B-08, GDITL Tower, Netaji Subhash Palace Delhi  110034<br><br>
+              <strong>Phone:</strong> +91-9899783871, +91-9811158245<br>
+              <strong>Email:</strong> vaibhavjainca.2012@gmail.com, kkjainca@gmail.com<br>
+            </p>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="index.html">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="about-us.html">About us</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="our-team.html">Our Team</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="index.html#services">Services</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="contact-us.html">Contact us</a></li>
+             
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="services.html#start-up-services">Start-up services</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="services.html#setting-up-of-business-in-india">Setting up of Business in India</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="services.html#direct-taxation">Direct Taxation</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="services.html#indirect-taxation">Indirect Taxation</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="services.html#auditing-and-risk-management">Auditing and Risk Management</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="services.html#legal-and-secretarial">Legal & Secretarial</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="services.html#accounting-and-business-support">Accounting and MIS Reporting</a></li>
+			  <li><i class="bx bx-chevron-right"></i> <a href="services.html#vcfo">VCFO</a></li>
+            </ul>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
---
+++ templates/header.php
@@ -0,0 +1,26 @@
+
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto"><a href="index.html"><img src="images/ca-logo.jpg"></a></h1>
+      
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a href="index.html" class="">Home</a></li>
+		  <li><a href="about-us.html">About us</a></li>
+		  <li><a href="index.html#how-we-work">How we Work</a></li>
+          <li><a href="our-team.html">Our Team</a></li>
+          <li><a href="index.html#services">Services</a></li>
+          <li><a href="index.html#why-work-with-us">Why Work With us</a></li>
+         
+          <li><a href="contact-us.html">Contact</a></li>
+
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>
+
+  
+
+    </div>
+  </header>
